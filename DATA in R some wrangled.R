2f913701425d5b57c2cc535a611b5a737b453eff
latrice <latrice.puckett@live.com>
2021-09-30 18:09:42 -0500

    more visuals

--- DATA in R some wrangled.R
+++ DATA in R some wrangled.R
@@ -1,7 +1,10 @@
 Data <- read.csv("C:/Users/latri/Desktop/SCI SCHOOLING/FINAL PROJECT/First Choice Data.csv", na.strings = c(""), stringsAsFactors=TRUE)
+#removed all unneeded columns
 data1 <- Data[ -c(5:8,10, 12:20,29:49,54) ]
-convictions= dataR[c(7:14)]
+#made a table with just prior convictions if needed, used recoded data for any needed analysis
+priors= dataR[c(7:14)]
 
+#all the packages!
 install.packages("ggplot2")
 library(ggplot2)
 
@@ -24,9 +27,12 @@ library("rcompanion")
 library("car")
 library("fastR2")
 
+#removing empty cells, looked like there were only 3277
 data2= na.omit(data1)
+
 str(data2)
 #everything is a factor with multiple levels, should be able to do some analysis and significant testing, or can recode data into numbers.
+#les is doing all other needed recodes. 
 dataR= mutate(data2,Prior_Conviction_Episodes_Felony=recode(Prior_Conviction_Episodes_Felony,"0"=0, "1"=1, "2"=2, "3 or more"= 3),
               Prior_Conviction_Episodes_Misd=recode(Prior_Conviction_Episodes_Misd,"0"=0, "1"=1, "2"=2, "3"= 3, "4 or more"= 4),
               Prior_Conviction_Episodes_Viol=recode(Prior_Conviction_Episodes_Viol, "true"=0, "false"=1), 
@@ -39,6 +45,7 @@ dataR= mutate(data2,Prior_Conviction_Episodes_Felony=recode(Prior_Conviction_Epi
               )
 
 
+#POST-WRANGLE INFO (checking that forms and types of data work for our analysis)
 #to make histograms have to use recoded data.
 ggplot(dataR, aes(x = Prior_Conviction_Episodes_GunCharges)) + geom_histogram(binwidth = 0.5)
 ggplot(dataR, aes(x = Prior_Conviction_Episodes_Misd)) + geom_histogram(binwidth = 0.5)
@@ -49,43 +56,28 @@ ggplot(data2, aes(Prior_Conviction_Episodes_Misd))+ geom_bar()
 barchart(data2$Age_at_Release)
 
 #cant check normality with categorical data
-ggplot(dataR, aes(sample = Prison_Offense)) + geom_qq()
+ggplot(dataR, aes(sample = Education_Level)) + geom_qq()
 
 
-
-#check correlation between some variables
-
+#check correlation between some variables?
 
 
 #DSO105 data time!
 #we could do some goodness of fit Chi squares with random guesses about probabilities of going back? perhaps need more background info about returning to prison
 #independent chi square to compare each! dont need to recode!use this to compare all the conviciton types to each other
-CrossTable(data2$Education_Level, data2$Prior_Conviction_Episodes_Viol, chisq = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
-CrossTable(data2$Education_Level, data2$Prison_Offense, chisq = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
+##CrossTable(data2$Education_Level, data2$Prior_Conviction_Episodes_Viol, chisq = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
+##CrossTable(data2$Education_Level, data2$Prison_Offense, chisq = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
 #for anything with only two fators
-CrossTable(data2$Gender, data1$Recidivism_Within_3years, chisq = TRUE,mcnemar = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
+##CrossTable(data2$Gender, data2$Recidivism_Within_3years, chisq = TRUE,mcnemar = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
 
 
 #homogeneity of variance tests, one way anova
-plotNormalHistogram(dataR$Education_Level)
-fligner.test(Education_Level ~ Prior_Conviction_Episodes_Misd, data=dataR)
+##plotNormalHistogram(dataR$Education_Level)
+##fligner.test(Education_Level ~ Prior_Conviction_Episodes_Misd, data=dataR)
+#perform post hocs find where significant
 #repeated measure anovas?
 
-#wondering if I should do the arrest years, education and age groups as well
+
 #want to ask joseph if we need to recode T/F data for analysis, or if recoding this data will affect our analysis for variance, or the counts of things.
-#can we analyze the T/F and the numbers data together. 
+#can we analyze the T/F and the numbers data together?
 #can use this data to compare with Recidivism within 3 years to ask "does having more prior convictions significantly affect the chances of being arrested again within three years?"
-
-#boxplot
-#ggplot(gapminder, aes(x = factor(year), y = pop)) + geom_boxplot()
-
-#line chart with title
-#ggplot(gm_Angola) + geom_line(aes(x = year, y = gdpPercap)) + ylab("Per Capita GDP") +  ggtitle("GDP in Angola")
-
-#line chart with color-coded data and data-sized points on chart
-#ggplot(gm_AfricaClean, aes(x = year, y = lifeExp, color = country)) + geom_line() + geom_point(aes(size = gdpPercap)) + ylab("Life Expectancy") + ggtitle("Life Expectancy and GDP in Four Countries")
-
-#scatterplot
-#ggplot(data1, aes(x = Age_at_Release, y = Prison_Offense, color =Recidivism_Within_3years )) + geom_point()
-
-#ggplot(convictions, aes(x = convictions)) + geom_histogram()
