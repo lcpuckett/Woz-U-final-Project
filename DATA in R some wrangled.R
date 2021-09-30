a18adfc1cad95f84f832407f0b1d7fc826458138
latrice <latrice.puckett@live.com>
2021-09-30 09:45:59 -0500

    day 3 week 2
    finally removed NAs

--- DATA in R some wrangled.R
+++ DATA in R some wrangled.R
@@ -1,6 +1,6 @@
-Data <- read.csv("C:/Users/latri/Desktop/SCI SCHOOLING/FINAL PROJECT/First Choice Data.csv", stringsAsFactors=TRUE)
+Data <- read.csv("C:/Users/latri/Desktop/SCI SCHOOLING/FINAL PROJECT/First Choice Data.csv", na.strings = c(""), stringsAsFactors=TRUE)
 data1 <- Data[ -c(5:8,10, 12:20,29:49,54) ]
-convictions= Data[c(21:28)]
+convictions= dataR[c(7:14)]
 
 install.packages("ggplot2")
 library(ggplot2)
@@ -14,22 +14,62 @@ library(tidyr)
 install.packages("tidyverse")
 library(tidyverse)
 
-data2= na.omit(data1)
-#after deleting all the columns we don't need, there are no missing values!
-#will keep using data1 from now on. 
+install.packages("lattice")
+library(lattice)
 
-str(data1)
+install.packages("gmodels")
+library("gmodels")
+
+library("rcompanion")
+library("car")
+library("fastR2")
+
+data2= na.omit(data1)
+str(data2)
 #everything is a factor with multiple levels, should be able to do some analysis and significant testing, or can recode data into numbers.
-data2= mutate(data1,Prior_Conviction_Episodes_Felony=recode(Prior_Conviction_Episodes_Felony,"0"=0, "1"=1, "2"=2, "3 or more"= 3),
+dataR= mutate(data2,Prior_Conviction_Episodes_Felony=recode(Prior_Conviction_Episodes_Felony,"0"=0, "1"=1, "2"=2, "3 or more"= 3),
               Prior_Conviction_Episodes_Misd=recode(Prior_Conviction_Episodes_Misd,"0"=0, "1"=1, "2"=2, "3"= 3, "4 or more"= 4),
               Prior_Conviction_Episodes_Viol=recode(Prior_Conviction_Episodes_Viol, "true"=0, "false"=1), 
               Prior_Conviction_Episodes_Prop=recode(Prior_Conviction_Episodes_Prop,"0"=0, "1"=1, "2"=2, "3 or more"= 3),
               Prior_Conviction_Episodes_Drug=recode(Prior_Conviction_Episodes_Drug,"0"=0, "1"=1, "2 or more"= 2),
               Prior_Conviction_Episodes_PPViolationCharges=recode(Prior_Conviction_Episodes_PPViolationCharges,"true"=0, "false"=1),
               Prior_Conviction_Episodes_DomesticViolenceCharges=recode(Prior_Conviction_Episodes_DomesticViolenceCharges,"true"=0, "false"=1),
-              Prior_Conviction_Episodes_GunCharges=recode(Prior_Conviction_Episodes_GunCharges,"true"=0, "false"=1)
+              Prior_Conviction_Episodes_GunCharges=recode(Prior_Conviction_Episodes_GunCharges,"true"=0, "false"=1),
+              Education_Level=recode(Education_Level,"At least some college"=3, "High School Diploma"=2, "Less than HS diploma"=1)
               )
 
+
+#to make histograms have to use recoded data.
+ggplot(dataR, aes(x = Prior_Conviction_Episodes_GunCharges)) + geom_histogram(binwidth = 0.5)
+ggplot(dataR, aes(x = Prior_Conviction_Episodes_Misd)) + geom_histogram(binwidth = 0.5)
+
+#can make bar charts without recode!
+ggplot(data2, aes(Age_at_Release))+ geom_bar()
+ggplot(data2, aes(Prior_Conviction_Episodes_Misd))+ geom_bar()
+barchart(data2$Age_at_Release)
+
+#cant check normality with categorical data
+ggplot(dataR, aes(sample = Prison_Offense)) + geom_qq()
+
+
+
+#check correlation between some variables
+
+
+
+#DSO105 data time!
+#we could do some goodness of fit Chi squares with random guesses about probabilities of going back? perhaps need more background info about returning to prison
+#independent chi square to compare each! dont need to recode!use this to compare all the conviciton types to each other
+CrossTable(data2$Education_Level, data2$Prior_Conviction_Episodes_Viol, chisq = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
+CrossTable(data2$Education_Level, data2$Prison_Offense, chisq = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
+#for anything with only two fators
+CrossTable(data2$Gender, data1$Recidivism_Within_3years, chisq = TRUE,mcnemar = TRUE, expected = TRUE, sresid=TRUE, format="SPSS")
+
+
+#homogeneity of variance tests, one way anova
+plotNormalHistogram(dataR$Education_Level)
+fligner.test(Education_Level ~ Prior_Conviction_Episodes_Misd, data=dataR)
+#repeated measure anovas?
 
 #wondering if I should do the arrest years, education and age groups as well
 #want to ask joseph if we need to recode T/F data for analysis, or if recoding this data will affect our analysis for variance, or the counts of things.
