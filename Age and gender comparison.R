0df33a066b2030eb79b2daf741bffb4c4a4e7550
Les Crone <ldcrone@gmail.com>
2021-10-03 10:15:28 -0500

    Add files via upload
    ## Updated to include Comparison of Gender and Age, Age and Recidivism_Within_3years, Recidivism and Education, Gender and Recidivism_Within_3years, and a first plot attempt of Age_at_Release and Recidivism_Within_3Years. 
    
    ## Some initial findings: (1) Recidivism for females is 45%, males is 59%, (2) There is a lower recidivism rate for people with some college, (3) Ages 23-27 and 27-32 have the highest recidivism rate
    
    ## My preliminary hypothesis is that if inmates in the 23-32 age bracket have an opportunity for additional education (above high school) before release, they will have a significantly lower risk of returning to prison
    
    ## We need to plot the normal distribution and test the hypothesis

--- Age and gender comparison.R
+++ Age and gender comparison.R
@@ -38,3 +38,29 @@ data2
 attach(data1)
 data2 <- table(Recidivism_Within_3years, Education_Level)
 data2
+
+## Comparison of Gender and Recidivism_Within_3years
+
+attach(data1)
+data2 <- table(Recidivism_Within_3years, Gender)
+data2
+
+## Recidivism for females is 45%, males is 59%
+
+## First plot attempt of Age_at_Release and Recidivism_Within_3Years
+
+df <- data.frame(Age_at_Release = (data1$Age_at_Release), 
+                 Recidivism_Within_3years = round(data1))
+
+library(ggplot2)
+ggplot(df, aes(x = Recidivism_Within_3years)) +
+  geom_histogram() + facet_grid(.~Age_at_Release)
+
+##  Plot needs some tweaking of the bin size. Latrice what do you think we can do to better show this plot?
+
+
+
+
+
+
+
